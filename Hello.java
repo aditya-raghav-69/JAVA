095b3531f4fd49606f398d04f41de359e7eb6b94
aditya-raghav-69 <aditya09969@gmail.com>
2025-03-03 02:02:12 +0530

    initial commit created repo and wrote java command line argument program

---
+++ Hello.java
@@ -0,0 +1,18 @@
+public class Hello{
+
+
+    public static void main(String[] args) {
+        
+        //Lets print some command line arguments
+
+    
+        for (int  i=0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
+
+
+
+
+
+    }
+}
