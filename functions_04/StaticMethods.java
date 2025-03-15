390e2f1de020d5dfb1860e18f1418b56fc54345f
aditya-raghav-69 <aditya09969@gmail.com>
2025-03-15 23:39:35 +0530

    added a basic program of static methods

---
+++ functions_04/StaticMethods.java
@@ -0,0 +1,11 @@
+
+
+public class StaticMethods {
+    public static void main(String[] args) {
+        System.out.println("The sum of 2 and 3 is " + sum(2, 3));
+    }
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+}
