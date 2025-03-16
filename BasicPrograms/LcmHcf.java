a8c9fc319703faee75f1fc1cae5a26c8c10fd393
aditya-raghav-69 <aditya09969@gmail.com>
2025-03-16 22:02:09 +0530

    created a program of lcm and hcf which need some optimization , will be done later

---
+++ BasicPrograms/LcmHcf.java
@@ -0,0 +1,63 @@
+package BasicPrograms;
+
+public class LcmHcf {
+
+    static int hcf(int a, int b) {
+
+
+        int ans;
+        if (a == b)
+            return a;
+        if (a < b)
+            ans = b;
+        else
+            ans = a;
+
+        
+        while (ans > 2) {
+            if (a % ans == 0 && b % ans == 0) {
+                break;
+            }
+            ans = ans - 1;
+        }
+
+        return ans;
+        
+
+    }
+
+    static int lcm(int a, int b) {
+        int ans;
+        if (a == b)
+            return a;
+        if (a < b)
+            ans = b;
+        else
+            ans = a;
+
+        
+        while (ans > 2) {
+            if (a % ans == 0 && b % ans == 0) {
+                break;
+            }
+            ans = ans + 1;
+        }
+        return ans;
+
+        
+    }
+
+    
+    public static void main(String[] args) {
+        
+        int a = 12, b = 44;
+
+        int hcf = hcf(a, b);
+
+        int lcm = lcm(a, b);
+
+        System.out.println("hcf : "+hcf);
+        System.out.println("lcm : "+lcm);
+
+    }
+}
