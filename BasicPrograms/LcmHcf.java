e1f085d10d9b97c26ec12b41bd7b65063aec51a0
aditya-raghav-69 <aditya09969@gmail.com>
2025-03-17 23:05:26 +0530

    fixed the error in the LcmHcf program

--- BasicPrograms/LcmHcf.java
+++ BasicPrograms/LcmHcf.java
@@ -1,19 +1,14 @@
-package BasicPrograms;
-
 public class LcmHcf {
 
     static int hcf(int a, int b) {
-
-
         int ans;
         if (a == b)
             return a;
-        if (a < b)
+        if (a > b)
             ans = b;
         else
             ans = a;
 
-        
         while (ans > 2) {
             if (a % ans == 0 && b % ans == 0) {
                 break;
@@ -22,42 +17,34 @@ public class LcmHcf {
         }
 
         return ans;
-        
-
     }
 
     static int lcm(int a, int b) {
         int ans;
         if (a == b)
             return a;
-        if (a < b)
-            ans = b;
-        else
+        if (a > b)
             ans = a;
+        else
+            ans = b;
 
-        
-        while (ans > 2) {
-            if (a % ans == 0 && b % ans == 0) {
+        while (true) {
+            if (ans % a == 0 && ans % b == 0) {
                 break;
             }
-            ans = ans + 1;
+            ans++;
         }
         return ans;
 
-        
     }
 
-    
     public static void main(String[] args) {
-        
         int a = 12, b = 44;
 
         int hcf = hcf(a, b);
-
         int lcm = lcm(a, b);
 
-        System.out.println("hcf : "+hcf);
-        System.out.println("lcm : "+lcm);
-
+        System.out.println("hcf : " + hcf);
+        System.out.println("lcm : " + lcm);
     }
 }
