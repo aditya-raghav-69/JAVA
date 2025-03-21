d738144a5a36b945ba5dbbc7a04cf79bf2d84b58
aditya-raghav-69 <aditya09969@gmail.com>
2025-03-21 22:13:22 +0530

    refactoring of code

--- ArraysAndArrayList_01/matrixAdd.java
+++ ArraysAndArrayList_01/matrixAdd.java
@@ -3,6 +3,20 @@ package ArraysAndArrayList_01;
 public class matrixAdd {
 
 
-static int[][] matrixAdd(int[][] matrix1, int[][] matrix2) {
+    static int[][] matrixAdd(int[][] matrix1, int[][] matrix2) {
+
+        int arr[][] = new int[matrix1.length][matrix1[0].length];
+
+
+
+
+
+
+        return arr;
+    }
+    public static void main(String[] args) {
+        
+        
+    }
     
 }
---
+++ functions_04/PrimeNum.java
@@ -0,0 +1,6 @@
+public class PrimeNum {
+    
+    public static void main(String[] args) {
+        
+    }
+}
