f8afea2cb5a8b4652aa4dee3b0299c548e6f6637
aditya-raghav-69 <aditya09969@gmail.com>
2025-03-22 23:41:17 +0530

    completed the armostrong program

--- functions_04/IsArmostrong.java
+++ functions_04/IsArmostrong.java
@@ -1,5 +1,20 @@
 public class IsArmostrong {
     public static void main(String[] args) {
+        int num = 153;
+
+        int temp = num;
+        int sum = 0;
+        while (temp > 0) {
+            int rem = temp % 10;
+            sum += rem * rem * rem;
+            temp /= 10;
+        }
+        if ( sum==num) {
+            System.out.println("Armstrong");
+        } else {
+            System.out.println("Not Armstrong");
+            
+        }
         
     }
     
