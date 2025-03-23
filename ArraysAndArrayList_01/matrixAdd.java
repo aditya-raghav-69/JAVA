b06f879514bc0dd5c1f149c062b10786291d0695
aditya-raghav-69 <aditya09969@gmail.com>
2025-03-23 23:11:45 +0530

    created some input variable for the function of the matrix return

--- ArraysAndArrayList_01/matrixAdd.java
+++ ArraysAndArrayList_01/matrixAdd.java
@@ -15,7 +15,16 @@ public class matrixAdd {
         return arr;
     }
     public static void main(String[] args) {
+
+
+        int arr[][] = matrixAdd(new int[][] { { 2, 4, 5 }, { 1, 2, 3 } }, new int[][] { { 6, 7, 8 }, { 2, 3, 4 } });
         
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
         
     }
     
