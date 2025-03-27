2d2347b6fc12096c53d535426e03fda27c3dee5f
aditya-raghav-69 <aditya09969@gmail.com>
2025-03-27 16:36:22 +0530

    solved the transpose matrix

--- ArraysAndArrayList_01/TransposeMatrix.java
+++ ArraysAndArrayList_01/TransposeMatrix.java
@@ -7,18 +7,53 @@ public class TransposeMatrix {
 
 
     static void transpose(int[][] matrix) {
-        
-    }
-    public static void main(String[] args) {
-        // matrix defined for passing to the function
-        int[][] matrix = { { 2, 4, 5 }, { 1, 2, 3 }, { -22, 34, 32 } };
-        
+
         // printing the matrix
+        System.out.println("The matrix before transposing is: ");
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
                 System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
         }
+
+        // transposing the matrix
+        int[][] arr = new int[matrix[0].length][matrix.length];
+        int row = matrix.length;
+        int col = matrix[0].length;
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                arr[j][i] = matrix[i][j];
+            }
+        }
+
+
+        // printing the transposed matrix
+        System.out.println("The matrix after transposing is: ");
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        
+        }
+
+
+
+
+
+
+
+
+
+        
     }
+    public static void main(String[] args) {
+        // matrix defined for passing to the function
+        int[][] matrix = { { 2, 4, 5 }, { 1, 2, 3 }, { -22, 34, 32 } };
+        
+        transpose(matrix);
+        }
+    
 }
